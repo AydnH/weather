c4735477dec4c01e1078fec85ffdd86fbddbe76f
Aydn <aydn@kicker-matchday.com>
2021-03-20 13:53:27 +0100

    First commit
    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# weather
+curl --request GET --url \
+'https://data.climacell.co/v4/timelines?location=-49.40768,8.69079&fields=temperature&timesteps=1h&units=metric&apikey=4ZkR31YytFTVWjfHWKLec2nxVOEWoBQf'
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ 
+      background: '#1E1E1E',
+      height: '100vh'
+    }}>
+      <a style={{ color: '#D8D8D8' }}> 
+      hello world
+      </a>
+    </div>
+  );
+}
+
+export default App;
